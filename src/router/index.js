bfd0a5f06fe7024d48cf2be3499ac771d544a708
mariam <mariam.rasho99@gmail.com>
2024-05-23 12:14:20 +0300

    edit files

--- src/router/index.js
+++ src/router/index.js
@@ -4,41 +4,41 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [{
     path: '/',
     component: () =>
-        import ('@/layouts/default/Default.vue'),
+        import('@/layouts/default/Default.vue'),
     children: [{
-            path: '/',
-            name: 'Home',
-            component: () =>
-                import ('@/views/Home.vue'),
-        }, {
-            path: 'About',
-            name: 'About',
-            component: () =>
-                import ('@/views/About.vue'),
-        },
-        {
-            path: '/Skills',
-            name: 'Skills',
-            component: () =>
-                import ('@/views/Skills.vue'),
-        },
-        {
-            path: '/Project',
-            name: 'Project',
-            component: () =>
-                import ('@/views/Project.vue'),
-        }, {
-            path: 'Contact',
-            name: 'Contact',
-            component: () =>
-                import ('@/views/ContactMe.vue'),
-        },
-    ],
-}, ]
+        path: '/',
+        name: 'Home',
+        component: () =>
+            import('@/views/Home.vue'),
+        // }, {
+        //     path: 'About',
+        //     name: 'About',
+        //     component: () =>
+        //         import ('@/views/About.vue'),
+        // },
+        // {
+        //     path: '/Skills',
+        //     name: 'Skills',
+        //     component: () =>
+        //         import ('@/views/Skills.vue'),
+        // },
+        // {
+        //     path: '/Project',
+        //     name: 'Project',
+        //     component: () =>
+        //         import ('@/views/Project.vue'),
+        // }, {
+        //     path: 'Contact',
+        //     name: 'Contact',
+        //     component: () =>
+        //         import ('@/views/ContactMe.vue'),
+        // },
+        // ],
+    },]
 
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes,
-})
+            const router = createRouter({
+        history: createWebHistory(process.env.BASE_URL),
+        routes,
+    })
 
-export default router
+            export default router
